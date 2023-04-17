34e58306f2913333f5f0822c1c9bf0bbfaa8e91e
Mizuki Akimoto <Mizuki.Akimoto@vanarts.com>
2023-04-17 16:02:06 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,216 @@
+let response = {
+  "kind": "youtube#searchListResponse",
+  "etag": "qD1YByp2etIwKX1M2HlHDNxsjyM",
+  "nextPageToken": "CAYQAA",
+  "regionCode": "CA",
+  "pageInfo": {
+    "totalResults": 7,
+    "resultsPerPage": 6
+  },
+  "items": [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "kfi4bYycbFDIV_ZP9g2k3kCfUhE",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "aoZCNEz-_jE"
+      },
+      "snippet": {
+        "publishedAt": "2023-01-12T17:25:07Z",
+        "channelId": "UCDvVhR8Bnd3SlSGinJaVoJA",
+        "title": "Seeking attention - little puppy wants mom to pet him",
+        "description": "The voice over is spot on and I wish pets can talk!!",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/aoZCNEz-_jE/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/aoZCNEz-_jE/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/aoZCNEz-_jE/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "funnypets101",
+        "liveBroadcastContent": "none",
+        "publishTime": "2023-01-12T17:25:07Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "vOEHJ3RcCLpUObqHL9jDTqIjYaI",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "dWr6sEznf6k"
+      },
+      "snippet": {
+        "publishedAt": "2023-01-12T17:24:07Z",
+        "channelId": "UCDvVhR8Bnd3SlSGinJaVoJA",
+        "title": "Dog Whisperer??",
+        "description": "He knows when to go quiet. Cutest reaction ever!",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/dWr6sEznf6k/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/dWr6sEznf6k/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/dWr6sEznf6k/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "funnypets101",
+        "liveBroadcastContent": "none",
+        "publishTime": "2023-01-12T17:24:07Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "2rAzQTqVIYmtc2FjMZToc1JvUKA",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "X9r_brrXW48"
+      },
+      "snippet": {
+        "publishedAt": "2023-01-12T17:23:16Z",
+        "channelId": "UCDvVhR8Bnd3SlSGinJaVoJA",
+        "title": "Adam waiting for this moment...",
+        "description": "Mom thought to bring Adam for a day at the river and did not expect the turn of events.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/X9r_brrXW48/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/X9r_brrXW48/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/X9r_brrXW48/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "funnypets101",
+        "liveBroadcastContent": "none",
+        "publishTime": "2023-01-12T17:23:16Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "XtB7JNKbsFiTZDcDaAlsLmHU5zM",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "isariXFqXMU"
+      },
+      "snippet": {
+        "publishedAt": "2023-01-12T17:21:19Z",
+        "channelId": "UCDvVhR8Bnd3SlSGinJaVoJA",
+        "title": "Smartest Golden Retriever ever!!",
+        "description": "He knows how to get the whole plate of food. So strategic.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/isariXFqXMU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/isariXFqXMU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/isariXFqXMU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "funnypets101",
+        "liveBroadcastContent": "none",
+        "publishTime": "2023-01-12T17:21:19Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "yLUfSasZaHwkiBakIhHEiM3XH4o",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "m-Eoz-DA0a4"
+      },
+      "snippet": {
+        "publishedAt": "2023-01-12T17:19:51Z",
+        "channelId": "UCDvVhR8Bnd3SlSGinJaVoJA",
+        "title": "Kate left Eugene for Jason",
+        "description": "Jason doesn't seem too interested.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/m-Eoz-DA0a4/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/m-Eoz-DA0a4/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/m-Eoz-DA0a4/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "funnypets101",
+        "liveBroadcastContent": "none",
+        "publishTime": "2023-01-12T17:19:51Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "oo1Ts3WXoYNgtF7pH4uK2_hjfFo",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "k4K4-YJ6sDw"
+      },
+      "snippet": {
+        "publishedAt": "2023-01-12T04:24:52Z",
+        "channelId": "UCDvVhR8Bnd3SlSGinJaVoJA",
+        "title": "Best Massage Ever!",
+        "description": "Grown pitbull enjoys a soulful massage session. Thank you, mom.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/k4K4-YJ6sDw/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/k4K4-YJ6sDw/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/k4K4-YJ6sDw/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "funnypets101",
+        "liveBroadcastContent": "none",
+        "publishTime": "2023-01-12T04:24:52Z"
+      }
+    }
+  ]
+}
